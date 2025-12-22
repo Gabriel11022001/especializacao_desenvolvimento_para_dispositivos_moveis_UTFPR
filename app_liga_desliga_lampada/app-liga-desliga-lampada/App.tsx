8eb9e8a6cf1322068615e1e77d5837f1122fef6f
Gabriel Rodrigues dos Santos <gabriel996567521@gmail.com>
2025-12-22 10:16:06 -0300

    mudei para utilizar led ao invés de lâmpada

--- app_liga_desliga_lampada/app-liga-desliga-lampada/App.tsx
+++ app_liga_desliga_lampada/app-liga-desliga-lampada/App.tsx
@@ -48,8 +48,8 @@ export default function App() {
   return (
     <SafeAreaView style={ { flex: 1, padding: 20, backgroundColor: "#323232", alignItems: "center" } }>
       <ScrollView style={ { flex: 1 } }>
-        <Image style={  styles.imgLampada } source={ statusLampada === StatusLampada.ligado ? require("./assets/images/lampada_ligada.png")
-          : require("./assets/images/lampada_desligada.png")
+        <Image style={  styles.imgLampada } source={ statusLampada === StatusLampada.ligado ? require("./assets/images/led_ligado.png")
+          : require("./assets/images/led_desligado.png")
          } />
          <TouchableOpacity 
           style={ styles.btnLampada }
