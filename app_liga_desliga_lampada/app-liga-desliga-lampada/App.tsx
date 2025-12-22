fa33c2d5a5debda1430eafba897493135ab4d861
Gabriel Rodrigues dos Santos <gabriel996567521@gmail.com>
2025-12-22 10:19:12 -0300

    mudei para utilizar led ao invés de lâmpada

--- app_liga_desliga_lampada/app-liga-desliga-lampada/App.tsx
+++ app_liga_desliga_lampada/app-liga-desliga-lampada/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Image, ScrollView, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-enum StatusLampada {
+enum StatusLed {
 
   ligado,
   desligado
@@ -14,33 +14,27 @@ const IP_ARDUINO = "192.168.0.121";
 
 export default function App() {
 
-  const [ statusLampada, setStatusLampada ] = useState<StatusLampada>(StatusLampada.desligado);
+  const [ statusLed, setStatusLed ] = useState<StatusLed>(StatusLed.desligado);
 
-  const apresentarAlertaErro = (msg: string): void => {
-
-  }
-
-  const acionarLampada = async () => {
+  const acionarLed = async () => {
 
     try {
 
-      if (statusLampada === StatusLampada.ligado) {
+      if (statusLed === StatusLed.ligado) {
         // desligar
-        setStatusLampada(StatusLampada.desligado);
+        setStatusLed(StatusLed.desligado);
 
         await fetch(`http://${ IP_ARDUINO }/desligar`);
       } else {
         // ligar
-        setStatusLampada(StatusLampada.ligado);
+        setStatusLed(StatusLed.ligado);
 
         await fetch(`http://${ IP_ARDUINO }/ligar`);
       }
 
-      console.log("Lâmpada acionada com sucesso!");
+      console.log("Led acionado com sucesso!");
     } catch (e) {
-      console.error("Erro ao tentar-se acionar a lâmpada: " + e);
-      
-      apresentarAlertaErro(`Erro ao tentar-se acionar a lâmpada: ${ e }`);
+      console.error("Erro ao tentar-se acionar o led: " + e);
     }
 
   }
@@ -48,13 +42,13 @@ export default function App() {
   return (
     <SafeAreaView style={ { flex: 1, padding: 20, backgroundColor: "#323232", alignItems: "center" } }>
       <ScrollView style={ { flex: 1 } }>
-        <Image style={  styles.imgLampada } source={ statusLampada === StatusLampada.ligado ? require("./assets/images/led_ligado.png")
+        <Image style={  styles.imgLed } source={ statusLed === StatusLed.ligado ? require("./assets/images/led_ligado.png")
           : require("./assets/images/led_desligado.png")
          } />
          <TouchableOpacity 
-          style={ styles.btnLampada }
-          onPress={ acionarLampada } >
-          <Text style={ styles.txtBtnLampada }>{ statusLampada === StatusLampada.ligado ? "Desligar" : "Ligar" }</Text>
+          style={ styles.btnLed }
+          onPress={ acionarLed } >
+          <Text style={ styles.txtBtnLed }>{ statusLed === StatusLed.ligado ? "Desligar" : "Ligar" }</Text>
          </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
@@ -62,13 +56,13 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  imgLampada: {
+  imgLed: {
     width: 300,
     height: 300,
     resizeMode: "contain",
     marginTop: 50
   },
-  btnLampada: {
+  btnLed: {
     backgroundColor: "#fff",
     padding: 20,
     borderRadius: 20,
@@ -77,7 +71,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  txtBtnLampada: {
+  txtBtnLed: {
     color: "#000",
     fontSize: 20,
     fontWeight: "bold",
